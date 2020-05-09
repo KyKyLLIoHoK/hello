ac3553aeb4c0ebf35ba558c9c7df0c6171d91f7d
KyKyLLIoHoK <KyKyLLIoHoK11@gmail.com>
2020-05-09 20:20:39 +0300

    test commit

--- src/app/hello.java
+++ src/app/hello.java
@@ -9,7 +9,7 @@ public class hello {
     public static void main(String[] args) {
         
         System.out.println("Hello, World");
-        scanner.next();
+       // scanner.next();
 
     }
 
