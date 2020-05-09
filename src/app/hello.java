ca3d8145572c25b9c0ff91732ce7692d62c0fbb9
KyKyLLIoHoK <KyKyLLIoHoK11@gmail.com>
2020-05-09 20:19:14 +0300

    Begin proj

---
+++ src/app/hello.java
@@ -0,0 +1,19 @@
+package src.app;
+import java.util.Scanner;
+
+
+public class hello {
+    static Scanner scanner = new Scanner(System.in);
+    
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello, World");
+        scanner.next();
+
+    }
+
+ 
+   
+}
+
